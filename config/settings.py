1b8b01838a41e39c0f0fa8a37f7bbf72902fc569
umarh <umar.hussain.da@outlook.com>
2021-02-20 03:14:46 +0000

    deployment changes

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ufbsvnlqckco4^b2*-6%w6_1clgv@0=3i64bc@wvl801%x16hc'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.herokuapp.com','localhost','127.0.0.1']
+ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
